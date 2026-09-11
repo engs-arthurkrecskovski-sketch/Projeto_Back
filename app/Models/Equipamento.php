4d8bbf562816812b031a64275a6451173f414e66
engs-jefersonpereira-collab <engs-jefersonpereira@camporeal.edu.br>
2026-09-11 11:04:35 -0300

    finalização do equipamento

--- app/Models/Equipamento.php
+++ app/Models/Equipamento.php
@@ -6,3 +6,27 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Equipamento extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'tipo',
+        'marca',
+        'modelo',
+        'numero_serie',
+        'descricao',
+    ];
+
+    public function cliente(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function ordensServico(): HasMany
+    {
+        return $this->hasMany(OrdemServico::class);
+    }
+}
