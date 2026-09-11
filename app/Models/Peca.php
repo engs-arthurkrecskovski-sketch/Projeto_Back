8e509bc4fed520af7fbeca975d485283167502ae
engs-jefersonpereira-collab <engs-jefersonpereira@camporeal.edu.br>
2026-09-11 11:10:06 -0300

    criação do model peça php

--- app/Models/Peca.php
+++ app/Models/Peca.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Peca extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'ordem_servico_id',
+        'nome',
+        'quantidade',
+        'valor_unitario',
+    ];
+
+    public function ordemServico(): BelongsTo
+    {
+        return $this->belongsTo(OrdemServico::class);
+    }
 }
