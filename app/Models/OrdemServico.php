6ad3c4602c9f1e2424848e6ca47ca705aeaf8e40
engs-jefersonpereira-collab <engs-jefersonpereira@camporeal.edu.br>
2026-09-11 11:10:52 -0300

    criacao do ordem e serviço

--- app/Models/OrdemServico.php
+++ app/Models/OrdemServico.php
@@ -2,9 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
-class OrdemServico extends Model
-{
-    //
-}
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
